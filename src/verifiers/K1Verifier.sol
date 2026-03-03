a7cb34d5c37244fa906814fffc311784e068de46
Conner Swenberg <conner.swenberg@coinbase.com>
2026-03-02 17:54:47 -0800

    Use OpenZeppelin ECDSA

--- src/verifiers/K1Verifier.sol
+++ src/verifiers/K1Verifier.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {ECDSA} from "openzeppelin/utils/cryptography/ECDSA.sol";
+
 import {IVerifier} from "./IVerifier.sol";
 
 contract K1Verifier is IVerifier {
@@ -13,14 +15,7 @@ contract K1Verifier is IVerifier {
         address owner = address(bytes20(ownerId));
         require(bytes32(bytes20(owner)) == ownerId);
 
-        // Signature must not be malleable
-        (uint8 v, bytes32 r, bytes32 s) = abi.decode(data, (uint8, bytes32, bytes32));
-        require(uint256(s) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0); // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/ECDSA.sol#L176-L184
-
-        // Recover address from signature
-        address recovered = ecrecover(hash, v, r, s);
-
-        // Verify recovered address matches ownerId
-        return recovered == owner;
+        // Verify recovered address matches owner
+        return owner == ECDSA.recover(hash, data);
     }
 }
